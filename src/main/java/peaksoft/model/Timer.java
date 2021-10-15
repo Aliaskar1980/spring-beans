0070691a0c9406db78e6ae1495de700e7b0332a3
aliaskarguur1980 <aliaskarguur1980@gmail.com>
2021-10-15 17:36:32 +0600

    added method getTimer, created class Dog, created beans for Timer and Dog classes

--- src/main/java/peaksoft/model/Timer.java
+++ src/main/java/peaksoft/model/Timer.java
@@ -1,10 +1,16 @@
 package peaksoft.model;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
+    private Long nanoTime1 = System.nanoTime();
 
     public Long getTime() {
         return nanoTime;
     }
+
+
 }
